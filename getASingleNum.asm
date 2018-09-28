35fded48d3e4deadde0ce6e0231c3d9162f85f24
ALi <s.arme@ya.ru>
2018-09-28 23:43:01 +0330

    implementing read function

--- getASingleNum.asm
+++ getASingleNum.asm
@@ -1,6 +1,6 @@
 global _start
 section .data ;uninitialized data
-inp_buf resb 5 ;reserves 5 bytes
+inp_buf resb 2 ;reserves 5 bytes
 msg1 db "You entered: "
 len1 equ $ - msg1
 msg2 db "Please enter x,y",0xa
@@ -13,6 +13,12 @@ mov ebp,esp ;initializing the stack
 push len2
 push msg2 ;passing arg to the stack
 call write
+push 2 ;length
+push inp_buf ;buffer
+call read
+push 2
+push inp_buf
+call write
 mov eax,1 ;sys_exit system calls
 mov ebx,0 ;exit status is 0
 int 0x80
@@ -20,13 +26,16 @@ int 0x80
 read:
 push ebp ;function Prologue
 mov ebp,esp
+mov ecx,[ebp+8]
+mov edx,[ebp+12]
 mov eax, 3 ;sys_read system calls
 mov ebx, 0 ;stdout file descriptor
-mov ecx, inp_buf ;pointer to the first element
-mov edx, 5;get a five byte num
-pop ebp
-push inp_buf
 int 0x80
+; mov ecx, inp_buf ;pointer to the first element
+; mov edx, 5;get a five byte num
+mov esp,ebp
+pop ebp
+ret
 
 write:
 push ebp
